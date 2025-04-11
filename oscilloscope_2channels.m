fb3168072c3632496b3acd8c2fcbe673567e06c3
Petr Pojar <pojar.petr@seznam.cz>
2025-04-11 11:37:21 +0200

    finished 2 channels & measured and saved data

--- oscilloscope_2channels.m
+++ oscilloscope_2channels.m
@@ -3,6 +3,8 @@
 % Tektronix VISA is be used for communicating with instrument
 
 % Clear MATLAB workspace of any previous instrument connections
+NUM_SAMPLES = 1e5;
+
 instrreset;
 
 % change this to match oscilloscope address
@@ -24,9 +26,13 @@ fopen(myScope);
 % select channel
 fprintf(myScope, 'DAT:SOU CH1');
 
+fprintf(myScope, 'HOR:RECO 100000');
+
+fprintf(myScope, 'HOR:SCA 0.1 ');
+
 fprintf(myScope, 'DATA:START 1');
 
-fprintf(myScope, 'DATA:STOP 10000000');
+fprintf(myScope, 'DATA:STOP 100000');
 
 yOffset = query(myScope, 'WFMO:YOFF?');
 
@@ -41,9 +47,34 @@ fprintf(myScope, 'HEAD 0');
 fprintf(myScope, 'CURVE?');
 
 % Read the captured data as 8-bit integer data type
-data = fread(myScope, 10000000, "int8");
+data = fread(myScope, NUM_SAMPLES, "int8");
 
-data = (str2double(verticalScale) * (data)' ); % + str2double(yOffset)
+data_ch1 = (str2double(verticalScale) * (data)' ); % + str2double(yOffset)
+
+% SECOND CHANNEL --------------------------------------------------
+
+fprintf(myScope, 'DAT:SOU CH2');
+
+fprintf(myScope, 'DATA:START 1');
+
+fprintf(myScope, 'DATA:STOP 100000');
+
+yOffset = query(myScope, 'WFMO:YOFF?');
+
+verticalScale  = query(myScope,'WFMOUTPRE:YMULT?');
+
+fprintf(myScope, 'DATA:ENCDG RIBINARY');
+
+fprintf(myScope, 'WIDTH 1');
+
+fprintf(myScope, 'HEAD 0');
+
+fprintf(myScope, 'CURVE?');
+
+% Read the captured data as 8-bit integer data type
+data = fread(myScope, NUM_SAMPLES, "int8");
+
+data_ch2 = (str2double(verticalScale) * (data)' ); % + str2double(yOffset)
 
 % Clean up Close the connection
 fclose(myScope);
@@ -51,7 +82,10 @@ fclose(myScope);
 clear myScope;
 
 % Plot the acquired data and add axis labels
-plot(data); 
+%clf;
+plot(data_ch1);
+hold on
+plot(data_ch2);
 xlabel('Sample Index'); 
 ylabel('Volts');
 title('Waveform Acquired from Tektronix Oscilloscope');
